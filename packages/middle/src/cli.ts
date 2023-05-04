1e2fd2dd11ef37a01a63a661fdb3827b274b0e24
yanxlg <yanxlg2@yonghui.cn>
2023-05-04 23:31:41 +0800

    feat(init): 工程化初始化

--- packages/middle/src/cli.ts
+++ packages/middle/src/cli.ts
@@ -2,9 +2,9 @@ import { run } from "umi";
 
 run({
   presets: [
-    require.resolve("@umijs/max/dist/preset"),
-    require.resolve("./preset"),
-  ], // preset 默认是max的preset
+    require.resolve("@umijs/max/dist/preset"), // preset 默认是max的preset
+    require.resolve("./middle-preset"),
+  ],
 }).catch((e) => {
   console.error(e);
   process.exit(1);
