2715d9b316e3b05dfbdc8bfcf9b2a9ed69bc13f0
yanxlg <yanxlg2@yonghui.cn>
2023-05-19 00:25:36 +0800

    feat(StudioPanel): 支持dev模式

--- packages/plugin-asset-dev/src/index.ts
+++ packages/plugin-asset-dev/src/index.ts
@@ -2,7 +2,7 @@
  * @Author: yanxlg
  * @Date: 2023-05-01 21:15:00
  * @LastEditors: yanxlg
- * @LastEditTime: 2023-05-18 23:25:22
+ * @LastEditTime: 2023-05-19 00:25:17
  * @Description:
  * 检查是不是存在view.tsx|view.jsx 如果支持，表示组件在编辑器中和。view.js 支持。  __editMode 属性。如果有的话原属性直接传过来，不处理（editable、children等）。
  * meta.json | meta.ts | meta.tsx  支持default导出，支持 meta 属性导出。
@@ -24,19 +24,19 @@ export default (api: IApi) => {
   });
 
   api.modifyConfig((memo, { paths }) => {
-    memo.externals = {
-      ...memo.externals,
-      "@formily/reactive": "Formily.Reactive", // iframe中获取到的是父页面全局变量。为什么需要用父元素的
-      react: "React",
-      "react-dom": "ReactDOM",
-    };
-    memo.headScripts = [
-      `//unpkg.com/react@17.0.2/umd/react.production.min.js`,
-      `//unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js`,
-      `//unpkg.com/@formily/reactive@2.2.13/dist/formily.reactive.umd.${
-        isDev ? "development" : "production"
-      }.js`,
-    ];
+    // memo.externals = {
+    //   ...memo.externals,
+    //   "@formily/reactive": "Formily.Reactive", // iframe中获取到的是父页面全局变量。为什么需要用父元素的
+    //   react: "React",
+    //   "react-dom": "ReactDOM",
+    // };
+    // memo.headScripts = [
+    //   `//unpkg.com/react@17.0.2/umd/react.production.min.js`,
+    //   `//unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js`,
+    //   `//unpkg.com/@formily/reactive@2.2.13/dist/formily.reactive.umd.${
+    //     isDev ? "development" : "production"
+    //   }.js`,
+    // ];
     // 地址
     memo.routes = [
       {
