8555e8dda8e77ce5ae3f3536d86df17062ba2f35
yanxlg <yanxlg2@yonghui.cn>
2023-05-30 00:18:01 +0800

    feat(release-check): init

--- packages/release-check/src/index.ts
+++ packages/release-check/src/index.ts
@@ -8,37 +8,43 @@
  * Copyright (c) 2023 by yanxlg, All Rights Reserved.
  */
 import { chalk } from "@umijs/utils";
-import simpleGit from "simple-git";
+import { simpleGit } from "simple-git";
 
 const git = simpleGit();
 
 export async function run(mainBranch: string) {
   const { current: _current, all } = await git.branch(); // 是不是所有的远程分支都能拿到
   const current = process.env.CI_COMMIT_REF_NAME || _current;
+
+  // 获取所有master 的提交日志
+  const { all: logs } = await git.log({
+    from: `origin/${mainBranch}`,
+    to: `origin/${mainBranch}`,
+    format: "%H",
+  });
+  const logHashSet = new Set(logs);
+  console.log(logs);
+  let unMergedReleaseBranches = [];
   // 当前是release分支，执行检测
   if (/^release/.test(current)) {
-    const releaseSet = new Set<string>();
-    all.forEach((branch) => {
-      if (/^remotes\/origin\/release/.test(branch)) {
-        releaseSet.add(branch.replace(/^remotes\/origin\//, ""));
+    for (let branch of all) {
+      const branchName = branch.replace(/^remotes\/origin\//, "");
+      if (branchName === current) {
+        continue;
+      } else if (/^release/.test(branchName)) {
+        // 获取最后一次提交，查看最后一次提交是否在logs中
+        const { latest } = await git.log({
+          from: `origin/${branchName}`,
+          to: `origin/${branchName}`,
+          format: "%H",
+        });
+        console.log(latest);
+        if (!logHashSet.has(latest)) {
+          // 不包括当前分支
+          unMergedReleaseBranches.push(branchName);
+        }
       }
-    });
-    releaseSet.delete(current);
-
-    await git.checkout(mainBranch); // 切换到基础分支
-    const { all: mergedBranches } = await git.branch(["--merged"]);
-    // 查询baseBranch 的merge列表
-    const mergedBranchSet = new Set(
-      mergedBranches.map((_) => _.replace(/^remotes\/origin\//, ""))
-    );
-    console.log(mergedBranches);
-    let unMergedReleaseBranches = [];
-    releaseSet.forEach((target) => {
-      if (!mergedBranchSet.has(target)) {
-        unMergedReleaseBranches.push(target);
-      }
-    });
-    await git.checkout(_current); // 切换回原来的分支
+    }
     if (unMergedReleaseBranches.length > 0) {
       console.error(
         chalk.red(
