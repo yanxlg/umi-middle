03b4222184472a5d7c1745be6c491bfa6204ccbe
yanxlg <yanxlg2@yonghui.cn>
2023-05-30 13:50:02 +0800

    feat(release-check): code update

--- packages/release-check/src/index.ts
+++ packages/release-check/src/index.ts
@@ -18,14 +18,11 @@ export async function run() {
 
   if (/^release/.test(current)) {
     const unMergedReleaseSet = new Set();
-
     // 不需要检测master，判断当前分支就可以
     for (let branch of all) {
       if (/^remotes\/origin\//.test(branch)) {
         const branchName = branch.replace(/^remotes\/origin\//, "");
-        if (branchName === current) {
-          continue;
-        } else if (/^release/.test(branchName)) {
+        if (/^release/.test(branchName) && branchName !== current) {
           // 获取最后一次提交，查看最后一次提交是否在logs中
           const { total } = await git.log({
             from: `origin/${current}`,
@@ -50,43 +47,4 @@ export async function run() {
       process.exit(1);
     }
   }
-
-  // 当前是release分支，执行检测
-  //   if (/^release/.test(current)) {
-  //     // 获取所有master 的提交日志
-  //     const { all: logs } = await git.log({
-  //       from: `origin/${current}`,
-  //     });
-  //     const logHashSet = new Set(logs.map((_) => _.hash));
-  //     const unMergedReleaseSet = new Set();
-
-  //     // 不需要检测master，判断当前分支就可以
-  //     for (let branch of all) {
-  //       if (/^remotes\/origin\//.test(branch)) {
-  //         const branchName = branch.replace(/^remotes\/origin\//, "");
-  //         if (branchName === current) {
-  //           continue;
-  //         } else if (/^release/.test(branchName)) {
-  //           // 获取最后一次提交，查看最后一次提交是否在logs中
-  //           const { latest } = await git.log({
-  //             from: `origin/${branchName}`,
-  //           });
-  //           if (!logHashSet.has(latest.hash)) {
-  //             unMergedReleaseSet.add(branchName);
-  //           }
-  //         }
-  //       }
-  //     }
-  //     let unMergedReleaseBranches = Array.from(unMergedReleaseSet);
-  //     if (unMergedReleaseBranches.length > 0) {
-  //       console.error(
-  //         chalk.red(
-  //           `存在release分支代码未合并到当前分支（上线后没有合并到master），新的release分支代码不完整，请处理完下列分支合并操作后重新发布编译动作：${chalk.blueBright(
-  //             unMergedReleaseBranches.join(" ")
-  //           )}`
-  //         )
-  //       );
-  //       process.exit(1);
-  //     }
-  //   }
 }
