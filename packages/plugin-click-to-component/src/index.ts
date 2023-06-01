dba54d9e5086dc28e267128c20f440e741f75d32
yanxlg <yanxlg2@yonghui.cn>
2023-06-01 12:40:06 +0800

    feat(支持webstorm): click-to-component 支持webstorm

--- packages/plugin-click-to-component/src/index.ts
+++ packages/plugin-click-to-component/src/index.ts
@@ -1,23 +1,29 @@
-import { dirname } from 'path';
-import { IApi } from "umi";
+import {dirname} from 'path';
+import {IApi} from "umi";
 
 export default (api: IApi) => {
-  api.describe({
-    key: 'clickToComponentPro',
-    enableBy: api.env === 'development' ? api.EnableBy.register : () => false,
-  });
+    api.describe({
+        key: 'clickToComponentPro',
+        enableBy: api.env === 'development' ? api.EnableBy.register : () => false,
+    });
 
-  const pkgPath = dirname(require.resolve('click-to-react-component-2'));
-  api.modifyConfig((memo) => { // 强制修改click-to-component 中的alias
-    memo.alias['click-to-react-component'] = pkgPath;
-    return memo;
-  });
+    const pkgPath = dirname(require.resolve('click-to-react-component-2'));
+    api.modifyConfig((memo) => { // 强制修改click-to-component 中的alias
+        const identifier = process.env.__CFBundleIdentifier || '';
+        if (memo.clickToComponent) {
+            if (!memo.clickToComponent.editor || memo.clickToComponent.editor === 'auto') {
+                memo.clickToComponent.editor = /webstorm/.test(identifier.toLowerCase()) ? 'webstorm' : 'vscode';
+            }
+        }
+        memo.alias['click-to-react-component'] = pkgPath;
+        return memo;
+    });
 
-  api.modifyAppData((memo) => {// 强制修改版本
-    memo.clickToComponent = {
-      pkgPath,
-      version: '1.0.11',
-    };
-    return memo;
-  });
+    api.modifyAppData((memo) => {// 强制修改版本
+        memo.clickToComponent = {
+            pkgPath,
+            version: '1.0.11',
+        };
+        return memo;
+    });
 };
