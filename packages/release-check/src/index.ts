679f60544add0e7a9ecb80f046067219983a83c3
yanxlg <yanxlg2@yonghui.cn>
2023-07-11 20:12:50 +0800

    feat(release-check): release分支检测优化检测效率

--- packages/release-check/src/index.ts
+++ packages/release-check/src/index.ts
@@ -9,8 +9,26 @@
  */
 import { chalk } from "@umijs/utils";
 import { simpleGit } from "simple-git";
+import dayjs from 'dayjs';
 
 const git = simpleGit({ baseDir: process.cwd() });
+
+
+function getDate(branchName: string){
+  const dateStr = branchName.replace(/[^\d]/g,'');
+  const length = dateStr.length;
+  const currentYear = new Date().getFullYear() + '';
+  switch (length) {
+    case 8:
+      return dateStr;
+    case 6: // 没有20
+      return currentYear.substring(0,2) + dateStr;
+    case 4:
+      return currentYear + dateStr;
+  }
+  return dayjs().format('YYYYMMDD');
+}
+
 
 export async function run() {
   const { current: _current, all } = await git.branch(); // 是不是所有的远程分支都能拿到
@@ -18,23 +36,38 @@ export async function run() {
 
   if (/^release/.test(current)) {
     const unMergedReleaseSet = new Set();
+
+    let prevReleaseBranch = '';
+
     // 不需要检测master，判断当前分支就可以
     for (let branch of all) {
       if (/^remotes\/origin\//.test(branch)) {
         const branchName = branch.replace(/^remotes\/origin\//, "");
+        // 获取当前分支对应的日期
+        const currentDate = getDate(current);
+
         if (/^release/.test(branchName) && branchName !== current) {
-          // 获取最后一次提交，查看最后一次提交是否在logs中
-          const { total } = await git.log({
-            from: `origin/${current}`,
-            to: `origin/${branchName}`,
-            symmetric: false,
-          });
-          if (total > 0) {
-            unMergedReleaseSet.add(branchName);
+          // 解析release 分支对应的日期
+          const branchDate = getDate(branchName);
+          if(dayjs(branchDate).isBefore(currentDate,'d') && (!prevReleaseBranch || dayjs(getDate(prevReleaseBranch)).isBefore(branchDate,'d'))){ // 前置日期，不需要检查所有，检查前一个就行
+            prevReleaseBranch = branchName;
           }
         }
       }
     }
+
+    if(prevReleaseBranch){
+      // 获取最后一次提交，查看最后一次提交是否在logs中
+      const { total } = await git.log({
+        from: `origin/${current}`,
+        to: `origin/${prevReleaseBranch}`,
+        symmetric: false,
+      });
+      if (total > 0) {
+        unMergedReleaseSet.add(prevReleaseBranch);
+      }
+    }
+
     let unMergedReleaseBranches = Array.from(unMergedReleaseSet);
     if (unMergedReleaseBranches.length > 0) {
       console.error(
