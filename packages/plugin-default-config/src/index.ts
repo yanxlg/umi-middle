134bf904fb66688821f2150f019ed45b27ef8d2a
81087708 <yanxl2@yonghui.cn>
2024-01-05 17:27:06 +0800

    feat(pageConvertRule): 增加configs、utils目录排除解析

--- packages/plugin-default-config/src/index.ts
+++ packages/plugin-default-config/src/index.ts
@@ -21,6 +21,8 @@ export default (api: IApi) => {
         /\/components\//,
         /\/models\//,
         /\/services\//,
+        /\/configs\//,
+        /\/utils\//,
         /\/redux-slices\//,
         /README.md/i,
       ],
