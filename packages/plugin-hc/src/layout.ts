b08721bfc61b677fc8231f0041c445c67211ba5d
yanxianliang <yanxl2@yonghui.cn>
2024-01-10 21:35:29 +0800

    feat(hc): fix

--- packages/plugin-hc/src/layout.ts
+++ packages/plugin-hc/src/layout.ts
@@ -115,6 +115,7 @@ export function resolveLayout(api: IApi) {
       api.writeTmpFile({
         path: RUNTIME_TYPE_FILE_NAME,
         tplPath: join(tmpDir, "runtimeConfig.d.ts"),
+        context: {}
       });
     }
   });
