9de05bd59d55c4c043e143cbc457e991e3711e15
81087708 <yanxl2@yonghui.cn>
2024-04-15 15:31:30 +0800

    feat(hooks): 分页hooks 支持

---
+++ packages/hooks/src/index.tsx
@@ -0,0 +1,10 @@
+/*
+ * @Author: yanxlg
+ * @Date: 2023-05-27 18:11:50
+ * @LastEditors: yanxlg
+ * @LastEditTime: 2023-05-29 11:43:33
+ * @Description:
+ *
+ * Copyright (c) 2023 by yanxlg, All Rights Reserved.
+ */
+export {default as usePagination} from './usePagination';
