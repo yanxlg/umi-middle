5b46ef40c283f2517ed739d3a1407392a6052948
81087708 <yanxl2@yonghui.cn>
2024-05-11 11:10:36 +0800

    feat('菜单'): appCode支持配置

--- packages/plugin-hc/template/runtimeConfig.d.ts
+++ packages/plugin-hc/template/runtimeConfig.d.ts
@@ -8,6 +8,7 @@ type LayoutType = {
   headerHeight?: number;
   layoutWrapper?: React.ClassType<any, any, any> | React.FunctionComponent;// Wrapper 组件，children 为Layout组件，可以向Layout传递props.
   patchClientMenus?: (menus:MenuItem[])=>MenuItem[];
+  menuAppCode?: string;
 };
 
 export interface IRuntimeConfig {
